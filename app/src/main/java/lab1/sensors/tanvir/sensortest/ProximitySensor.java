d0543f129deb9db04e90a1226f578b711d1a594c
Mohammad Tanvir Islam <tanvieer@gmail.com>
2017-03-21 17:53:41 +0600

    Proximity Sensor

--- app/src/main/java/lab1/sensors/tanvir/sensortest/ProximitySensor.java
+++ app/src/main/java/lab1/sensors/tanvir/sensortest/ProximitySensor.java
@@ -1,11 +1,21 @@
 package lab1.sensors.tanvir.sensortest;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
-public class ProximitySensor extends AppCompatActivity {
+public class ProximitySensor extends AppCompatActivity implements SensorEventListener{
+
+    private TextView _value;
+    private Sensor proximity;
+    private SensorManager SM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +24,36 @@ public class ProximitySensor extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+        _value = (TextView)findViewById(R.id.value);
 
 
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+        proximity = SM.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        SM.registerListener(this,proximity, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
 
     public void onBack(View v){
         startActivity(new Intent(this, MainActivity.class));
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        _value.setText("Value = "+Float.toString(event.values[0]));
+        Log.d("testSensor","ProximitySensor Value = "+Float.toString(event.values[0]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SM.unregisterListener(this, SM.getDefaultSensor(Sensor.TYPE_PROXIMITY));
+    }
+
 }
